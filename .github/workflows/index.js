14173762b4cb9eb0858150c89b183188268f72aa
DevAwizard <153505451+DevAwizard@users.noreply.github.com>
2024-03-22 16:36:53 +0100

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -14,11 +14,8 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### Hi there 👋
 
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
-
----
 
 ⏰ Updated on ${new Date().toUTCString()}
 
